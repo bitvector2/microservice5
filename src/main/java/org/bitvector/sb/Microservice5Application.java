efb498f0ad9ea67984fcd7f899ecc4fd557dbac3
Steven Logue <bitvector2@gmail.com>
2018-07-17 00:28:05 -0700

    initial commit

---
+++ src/main/java/org/bitvector/sb/Microservice5Application.java
@@ -0,0 +1,12 @@
+package org.bitvector.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservice5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Microservice5Application.class, args);
+    }
+}
